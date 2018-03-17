d46b501e140a58702797773c7754f26ca80e853a
Artur Wasik <wasikartur87@gmail.com>
2018-03-17 08:51:36 +0100

    mapping collections

--- Vega/Controllers/VehiclesController.cs
+++ Vega/Controllers/VehiclesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Vega.Controllers.Resources;
 using Vega.Models;
 using Vega.Persistence;
@@ -24,6 +25,7 @@ namespace Vega.Controllers
 		    this.mapper = mapper;
 	    }
 
+		[HttpPost]
 	    public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
 	    {
 		    if (!ModelState.IsValid)
@@ -41,5 +43,25 @@ namespace Vega.Controllers
 
 		    return Ok(result);
 	    }
-    }
+
+	    [HttpPut("{id}")]
+	    public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+	    {
+		    if (!ModelState.IsValid)
+		    {
+			    return BadRequest(ModelState);
+		    }
+
+		    var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+			mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+
+		    vehicle.LastUpdate = DateTime.Now;
+
+		    await context.SaveChangesAsync();
+
+		    var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+		    return Ok(result);
+	    }
+	}
 }
--- Vega/Mapping/MappingProfile.cs
+++ Vega/Mapping/MappingProfile.cs
@@ -8,24 +8,52 @@ using Vega.Models;
 
 namespace Vega.Mapping
 {
-    public class MappingProfile : Profile
-    {
-	    public MappingProfile()
-	    {
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
 			//Domain to API Resource
-		    CreateMap<Make, MakeResource>();
-		    CreateMap<Model, ModelResource>();
-		    CreateMap<Feature, FeatureResource>();
-		    CreateMap<Vehicle, VehicleResource>()
-			    .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource{ Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
-			    .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
+			CreateMap<Make, MakeResource>();
+			CreateMap<Model, ModelResource>();
+			CreateMap<Feature, FeatureResource>();
+			CreateMap<Vehicle, VehicleResource>()
+				.ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone }))
+				.ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
 
 			//API Resource to Domain
-		    CreateMap<VehicleResource, Vehicle>()
-			    .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
-			    .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
-			    .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
-			    .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id})));
-	    }
-    }
+			CreateMap<VehicleResource, Vehicle>()
+				.ForMember(v => v.Id, opt => opt.Ignore())
+				.ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+				.ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+				.ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+				.ForMember(v => v.Features, opt => opt.Ignore())
+				.AfterMap((vr, v) =>
+				{
+					// Remove unselected features
+					var removedFeatures = new List<VehicleFeature>();
+					foreach (var f in v.Features)
+					{
+						if (!vr.Features.Contains(f.FeatureId))
+						{
+							removedFeatures.Add(f);
+						}
+					}
+
+					foreach (var f in removedFeatures)
+					{
+						v.Features.Remove(f);
+					}
+
+					// Add new features
+					foreach (var id in vr.Features)
+					{
+						if (!v.Features.Any(f => f.FeatureId == id))
+						{
+							v.Features.Add(new VehicleFeature { FeatureId = id});
+						}
+					}
+
+				});
+		}
+	}
 }
