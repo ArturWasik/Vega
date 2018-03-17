c17751f776d45c5d854b192fc8f896bcf4435088
Artur Wasik <wasikartur87@gmail.com>
2018-03-17 16:57:24 +0100

    repository pattern

--- Vega/Controllers/VehiclesController.cs
+++ Vega/Controllers/VehiclesController.cs
@@ -18,9 +18,11 @@ namespace Vega.Controllers
 		private readonly IMapper mapper;
 
 	    private readonly VegaDbContext context;
+	    private readonly IVehicleRepository repository;
 
-		public VehiclesController(IMapper mapper, VegaDbContext context)
+	    public VehiclesController(IMapper mapper, VegaDbContext context, IVehicleRepository repository)
 		{
+			this.repository = repository;
 			this.context = context;
 		    this.mapper = mapper;
 	    }
@@ -41,12 +43,7 @@ namespace Vega.Controllers
 
 		    await context.Models.Include(m => m.Make).SingleOrDefaultAsync(m => m.Id == vehicle.ModelId);
 
-		    vehicle = await context.Vehicles
-			    .Include(v => v.Features)
-			    .ThenInclude(vf => vf.Feature)
-			    .Include(v => v.Model)
-			    .ThenInclude(m => m.Make)
-			    .SingleOrDefaultAsync(v => v.Id == vehicle.Id);
+		    vehicle = await repository.GetVehicle(vehicle.Id);
 
 			var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
@@ -61,12 +58,7 @@ namespace Vega.Controllers
 			    return BadRequest(ModelState);
 		    }
 
-		    var vehicle = await context.Vehicles
-			    .Include(v => v.Features)
-			    .ThenInclude(vf => vf.Feature)
-			    .Include(v => v.Model)
-			    .ThenInclude(m => m.Make)
-			    .SingleOrDefaultAsync(v => v.Id == id);
+		    var vehicle = await repository.GetVehicle(id);
 
 			if (vehicle == null)
 		    {
@@ -102,13 +94,8 @@ namespace Vega.Controllers
 		
 		[HttpGet("{id}")]
 	    public async Task<IActionResult> GetVehicle(int id)
-	    {
-		    var vehicle = await context.Vehicles
-			    .Include(v => v.Features)
-					.ThenInclude(vf => vf.Feature)
-				.Include(v => v.Model)
-					.ThenInclude(m => m.Make)
-			    .SingleOrDefaultAsync(v => v.Id == id);
+		{
+			var vehicle = await repository.GetVehicle(id);
 
 		    if (vehicle == null)
 		    {
