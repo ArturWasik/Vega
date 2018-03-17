4e1b5a6a0c3657d8019910bc417f465a9d3d982a
Artur Wasik <wasikartur87@gmail.com>
2018-03-17 11:27:45 +0100

    changing the return type of APIs

--- Vega/Controllers/VehiclesController.cs
+++ Vega/Controllers/VehiclesController.cs
@@ -39,7 +39,16 @@ namespace Vega.Controllers
 		    context.Vehicles.Add(vehicle);
 		    await context.SaveChangesAsync();
 
-		    var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+		    await context.Models.Include(m => m.Make).SingleOrDefaultAsync(m => m.Id == vehicle.ModelId);
+
+		    vehicle = await context.Vehicles
+			    .Include(v => v.Features)
+			    .ThenInclude(vf => vf.Feature)
+			    .Include(v => v.Model)
+			    .ThenInclude(m => m.Make)
+			    .SingleOrDefaultAsync(v => v.Id == vehicle.Id);
+
+			var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
 		    return Ok(result);
 	    }
@@ -52,9 +61,14 @@ namespace Vega.Controllers
 			    return BadRequest(ModelState);
 		    }
 
-		    var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+		    var vehicle = await context.Vehicles
+			    .Include(v => v.Features)
+			    .ThenInclude(vf => vf.Feature)
+			    .Include(v => v.Model)
+			    .ThenInclude(m => m.Make)
+			    .SingleOrDefaultAsync(v => v.Id == id);
 
-		    if (vehicle == null)
+			if (vehicle == null)
 		    {
 			    return NotFound();
 		    }
@@ -65,7 +79,7 @@ namespace Vega.Controllers
 
 		    await context.SaveChangesAsync();
 
-		    var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+		    var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
 		    return Ok(result);
 	    }
