b0cb5ad8366a02f675fcacf1adf474eb1aa062c5
Artur Wasik <wasikartur87@gmail.com>
2018-03-18 20:03:39 +0100

    filters on the server

--- Vega/ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ Vega/ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -10,7 +10,6 @@ import { IKeyValuePair } from '../../models/shared';
 })
 export class VehicleListComponent implements OnInit {
 	vehicles: IVehicle[];
-	allVehicles: IVehicle[];
 	makes: IKeyValuePair[];
 	filter: any = {};
 
@@ -21,22 +20,16 @@ export class VehicleListComponent implements OnInit {
 		this.vehicleService.getMakes()
 			.subscribe(makes => this.makes = makes);
 
-		this.vehicleService.getVehicles()
-			.subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
+		this.populateVehicles();
+	}
+
+	private populateVehicles() {
+		this.vehicleService.getVehicles(this.filter)
+			.subscribe(vehicles => this.vehicles = vehicles);
 	}
 
 	onFilterChange() {
-		var vehicles = this.allVehicles;
-
-		if (this.filter.makeId) {
-			vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
-		}
-
-		if (this.filter.modelId) {
-			vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
-		}
-
-		this.vehicles = vehicles;
+		this.populateVehicles();
 	}
 
 	resetFilter() {
