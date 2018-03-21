1035f636017a65cc6500fdb38254301c6712f660
Artur Wasik <wasikartur87@gmail.com>
2018-03-21 20:09:21 +0100

    upload progress

--- Vega/ClientApp/app/app.module.ts
+++ Vega/ClientApp/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, BrowserXhr } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ToastyModule } from "ng2-toasty";
 
@@ -18,6 +18,8 @@ import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.com
 
 import { VehicleService } from './services/vehicle.service';
 import { PhotoService } from './services/photo.service';
+import { ProgressService } from './services/progress.service';
+import { BrowserXhrWithProgress } from './services/progress.service';
 
 @NgModule({
 	declarations: [
@@ -50,8 +52,10 @@ import { PhotoService } from './services/photo.service';
 	],
 	providers: [
 		{ provide: ErrorHandler, useClass: AppErrorHandler },
+		{ provide: BrowserXhr, useClass: BrowserXhrWithProgress },
 		VehicleService,
-		PhotoService
+		PhotoService,
+		ProgressService
 	]
 })
 export class AppModuleShared {
